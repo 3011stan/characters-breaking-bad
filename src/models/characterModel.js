4141a066daa49ea77ec4ddb8264a6b0e8aed2a59
Stanrley <3011stan@gmail.com>
2022-01-24 13:43:45 -0300

    Criação do model inicial referente aos personagens: 1 - Insere personagem no banco, 2 - Busca personagem por id, 3 - Busca todos os personagens

---
+++ src/models/characterModel.js
@@ -0,0 +1,32 @@
+const connection = require('./connection');
+
+const createCharacterModel = async (character) => {
+  const createdCharacter = await connection().then((db) => db
+    .collection('characters').insertOne(character))
+    .then(({ insertedId }) => insertedId).catch((error) => console.log('--EROOU--' + error));
+
+  return createdCharacter;
+};
+
+const findByIdModel = async (id) => {
+  const query = { char_id: id };
+  const charFound = await connection().then((db) => db
+    .collection('characters').findOne(query))
+    .then((res) => res).catch((error) => console.log(error));
+
+  return charFound;
+}
+
+const getAllModel = async () => {
+  const allChars = await connection().then((db) => db
+    .collection('characters').find({}).toArray())
+    .then((res) => res).catch((error) => console.log(error));
+  
+  return allChars;
+}
+
+module.exports = {
+  createCharacterModel,
+  findByIdModel,
+  getAllModel,
+};
