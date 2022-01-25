3635099410f6e7f348d3176a3d6f88ab11974472
3011stan <3011stan@gmail.com>
2022-01-25 14:23:21 -0300

    Adição dotenv no arquivo server.js

--- src/api/server.js
+++ src/api/server.js
@@ -1,5 +1,6 @@
 const app = require('./app');
+require('dotenv').config();
 
-const { PORT } = process.env;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Conectado na porta ${PORT || 3000}`));
+app.listen(PORT, () => console.log(`Conectado na porta ${PORT}`));
