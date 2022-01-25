af021edb7911c1ef38545834c94f51fe219df390
3011stan <3011stan@gmail.com>
2022-01-25 13:48:49 -0300

    Fix port heroku

--- src/api/server.js
+++ src/api/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
+const { PORT } = process.env;
 
-app.listen(PORT, () => console.log(`Conectado na porta ${PORT}`));
+app.listen(PORT, () => console.log(`Conectado na porta ${PORT || 3000}`));
