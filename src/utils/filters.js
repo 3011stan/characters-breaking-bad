b151552458ccd760da3c196e1b492673d5024b24
Stanrley <3011stan@gmail.com>
2022-01-24 23:27:10 -0300

    Melhoria nos filtros: alguns episodios tem como personagens o nikcname e não o name, feito ajuste para pegar qualquer um dos dois

--- src/utils/filters.js
+++ src/utils/filters.js
@@ -1,12 +1,21 @@
 const { getAllEpisodes } = require("./useBreakingBadApi");
 
-const filterEpisodesByCharacters = async (character) => {
+const containCharacter = (allCharacters, char, nickname) => {
+  let exist = false;
+  allCharacters.forEach((participant) => {
+    if(participant.includes(char) || participant.includes(nickname)) {
+      exist = true;
+    }
+  });
+  return exist;
+}
+
+const filterEpisodesByCharacters = async (character, nickname) => {
   const allEpisodes = await getAllEpisodes();
   const filteredEpisodes = [];
-
   allEpisodes.forEach((ep) => {
     const {title, episode, season} = ep;
-    if (ep.characters.indexOf(character) > -1) {
+    if (containCharacter(ep.characters, character, nickname)) {
       filteredEpisodes.push({title, season, episode});
     }
   });
